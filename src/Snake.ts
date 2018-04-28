ba622c40a9324c61d3f7eacb2c7abb0da5224d79
Volodymyr Pauk <pauk.volodymyr@gmail.com>
2018-04-29 00:19:13 +0300

    Fixes

---
+++ src/Snake.ts
@@ -0,0 +1,18 @@
+import { Cube } from "./Shape/Cube";
+import { Shape } from "./Shape/Shape";
+
+export class Snake {
+    protected list: Array<Shape> = [];
+
+    constructor(protected context: CanvasRenderingContext2D) {
+        this.list = [];
+    }
+
+    public add(item: Shape): void {
+        this.list.push(item);
+    }
+
+    public getList(): Array<Shape> {
+        return this.list;
+    }
+}
