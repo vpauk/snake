6d22ec9eb7a85cd1dc3ec9a9a5e188454eae8dc0
Volodymyr Pauk <pauk.volodymyr@gmail.com>
2018-04-29 11:31:27 +0300

    Refactoring

--- src/Snake.ts
+++ src/Snake.ts
@@ -3,10 +3,6 @@ import { Section } from "./Section";
 export class Snake {
     protected list: Array<Section> = [];
 
-    constructor(protected context: CanvasRenderingContext2D) {
-        this.list = [];
-    }
-
     public add(item: Section): void {
         this.list.push(item);
     }
